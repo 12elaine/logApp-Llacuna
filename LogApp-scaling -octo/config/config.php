6af48cc8a314b3ee03fdbea4eb483bd04345eafe
12elaine <112542672+12elaine@users.noreply.github.com>
2023-04-18 22:55:03 +0800

    updated

---
+++ LogApp-scaling -octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/logApp-Llacuna\LogApp-scaling -octo');
+	define('DB_HOST', 'localhost:3307');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'loggapp_user');
